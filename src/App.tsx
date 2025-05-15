bae2733fa9f5655711a110d7687eb7c5735c2793
Arggusx <dougclasher9@gmail.com>
2025-05-15 09:18:53 -0300

    Cria botão de login e o modal.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import Dashboard from "./page/Dashdoard";
+import { useState } from "react";
+import LoginModal from '/home/arggusx/Documentos/Projetos/panelMenu/src/componentes/adm/loginModal.tsx';
 
-function App() {
+const App: React.FC = () =>  {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleLogin = (username: string, password: string) => {
+    console.log('Login solicitado:', username, password);
+    // aqui você pode adicionar sua lógica de autenticação
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="min-h-screen bg-gray-100">
       <nav className="bg-white shadow-sm">
-        <div className="container mx-auto px-4 py-3">
+        <div className="container mx-auto px-4 py-7 flex justify-between items-center">
           <h1 className="text-xl font-bold text-blue-600">Hamburgueria Admin</h1>
+      
         </div>
       </nav>
       <main>
         <Dashboard />
+        <LoginModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          onLogin={handleLogin}
+        />
       </main>
-    </div>
+</div>
   );
 }
 
